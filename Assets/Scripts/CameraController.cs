c5868a6ad07facfef22ef784210efefc7fc32be1
fuchinosuke <wenmrykl_1218@yahoo.co.jp>
2022-10-07 15:33:52 +0900

    コミット

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,33 +4,47 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject playerObject; //視点の対象
+    /*public GameObject playerObject; //視点の対象
     public GameObject plyerController;
     public float rotateSpeed = 2.0f; //回転の速さ
     private bool DebugCameraflag = false; //デバッグ用カメラ
+    private Vector3 initCameraposition;
+    private Vector3 targetpos;
+    private Vector3 targetrotate;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        initCameraposition = transform.position;
+        transform.position = playerObject.transform.position+initCameraposition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         //通常のカメラモード
         if(DebugCameraflag == false){
+            //プレイヤー追尾
+            //transform.position = playerObject.transform.position+initCameraposition;
             //Vector3でX,Y方向の回転の度合いを定義
             Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed,Input.GetAxis("Mouse Y") * rotateSpeed, 0);
     
-            //transform.RotateAround()をしようしてメインカメラを回転させる
-            transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
-            transform.RotateAround(playerObject.transform.position, transform.right, angle.y);
+            //transform.RotateAround()を使用してメインカメラを回転させる
+            transform.RotateAround(transform.position, Vector3.up, angle.x);
+            transform.RotateAround(transform.position, transform.right, angle.y);
+
+            //カメラの方向にプレイヤーを向かせたかった（遺言）
+            //playerObject.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
+            transform.position += playerObject.transform.position - targetpos;
+            targetpos = playerObject.transform.position;
+            transform.RotateAround(targetpos, Vector3.up, angle.x * Time.deltaTime);
         }
         else if(DebugCameraflag == true){
             //Vector3でX,Y方向の回転の度合いを定義
             Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed,Input.GetAxis("Mouse Y") * rotateSpeed, 0);
     
-            //transform.RotateAround()をしようしてメインカメラを回転させる
+            //transform.RotateAround()を利用してメインカメラを回転させる
             transform.RotateAround(transform.position, Vector3.up, angle.x);
             transform.RotateAround(transform.position, transform.right, angle.y);
             
@@ -54,13 +68,63 @@ public class CameraController : MonoBehaviour
             }
         }
     }
-    public void DebugCameramode(){
-        DebugCameraflag = !DebugCameraflag;
+    
+    }*/
+    private GameObject targetObj;
+    private Vector3 targetPos;
+    [SerializeField]private float rotatespeed = 400f;
+    private bool DebugCameraflag = false; //デバッグ用カメラ
+    private Vector3 initCameraposition;
+    void Start () {
+        targetObj = GameObject.Find("Player");
+        targetPos = targetObj.transform.position;
+        initCameraposition = transform.position;
+    }
+ 
+    void Update() {
         if(DebugCameraflag == false){
-            plyerController.GetComponent<PlayerController>().enabled = true;
+            // targetの移動量分、自分（カメラ）も移動する
+            transform.position += targetObj.transform.position - targetPos;
+            targetPos = targetObj.transform.position;
+            camerarotate();
         }
         else if(DebugCameraflag == true){
-            plyerController.GetComponent<PlayerController>().enabled = false;
+            camerarotate();
+            if(Input.GetKey(KeyCode.UpArrow)){
+                transform.position += transform.forward;
+            }
+            if(Input.GetKey(KeyCode.DownArrow)){
+                transform.position -= transform.forward;
+            }
+            if(Input.GetKey(KeyCode.RightArrow)){
+                transform.position += transform.right;
+            }
+            if(Input.GetKey(KeyCode.LeftArrow)){
+                transform.position -= transform.right;
+            }
+        }
+    }   
+    private void camerarotate(){
+        // マウスの右クリックを押している間
+        if (Input.GetMouseButton(0)) {
+            // マウスの移動量
+            float mouseInputX = Input.GetAxis("Mouse X");
+            float mouseInputY = Input.GetAxis("Mouse Y");
+            // targetの位置のY軸を中心に、回転（公転）する
+            transform.RotateAround(targetPos, Vector3.up, mouseInputX * Time.deltaTime * rotatespeed);
+            // カメラの垂直移動（※角度制限なし、必要が無ければコメントアウト）
+            transform.RotateAround(targetPos, transform.right, mouseInputY * Time.deltaTime * rotatespeed);
+        }
+    } 
+    public void DebugCameramode(){
+        var pc = GameObject.Find("PlayerController");
+        DebugCameraflag = !DebugCameraflag;
+        if(DebugCameraflag == false){
+            pc.GetComponent<PlayerController>().enabled = true;
+        }
+        else if(DebugCameraflag == true){
+            pc.GetComponent<PlayerController>().enabled = false;
+            transform.position = targetObj.transform.position+initCameraposition;
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,33 +4,65 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject Player;
-    private Rigidbody _rigidbody;
+    //ネット動作よくわからないので実体ポップできるようにコントローラー式にしてます
+    public GameObject Player; //追う対象 場合によってはfindで必要なもの探してください
+    private Rigidbody rb;
 
+    [SerializeField] private float speed = 0.1f;
+    [SerializeField] private float rotatespeed = 1f;
+    private Vector3 rv; //入力値の格納用
     void Start () {
-        _rigidbody = Player.GetComponent<Rigidbody>();
+        rb = Player.GetComponent<Rigidbody>();
+        //Player = GameObject.Find("Player");
     }
 
     void Update () {
-         //var speed = 5.0f;
+        //getAxis使うんならこれ(ただし十字キーでも判定するようになる)
+        //var speed = 5.0f;
         //var vx = speed * Input.GetAxis("Horizontal");
         //var vz = speed * Input.GetAxis("Vertical");
-        var rv = new Vector3();
+        rv = new Vector3();
+        //WASD押したら方向を代入
         if (Input.GetKey(KeyCode.A)) {
-                rv = new Vector3(-0.1f,0f,0f);
+                rv.x = -speed;
+                //Player.transform.Rotate(0, -rotatespeed, 0);
+                Player.transform.position += Player.transform.forward * speed;
             }
-            if (Input.GetKey(KeyCode.D)) {
-                rv = new Vector3(0.1f,0f,0f);
+        if (Input.GetKey(KeyCode.D)) {
+                rv.x = speed;
+                //Player.transform.Rotate(0, rotatespeed, 0);
+                Player.transform.position += Player.transform.forward * speed;
             }
-            if (Input.GetKey(KeyCode.W)) {
-                rv = new Vector3(0f,0f,0.1f);
+        if (Input.GetKey(KeyCode.W)) {
+                rv.z = speed;
+                Player.transform.position += Player.transform.forward * speed;
             }
-            if (Input.GetKey(KeyCode.S)) {
-                rv = new Vector3(0f,0f,-0.1f);
+        if (Input.GetKey(KeyCode.S)) {
+                rv.z = -speed;
+                Player.transform.position += Player.transform.forward * speed;
             }
-            
+        
+        //ここで動きを反映
         //_rigidbody.velocity = new Vector3(rv.x,_rigidbody.velocity.y,rv.z);
-        Player.transform.Translate(rv.x,0,rv.z);
+        //Player.transform.Translate(rv);
     }
-    
+    void FixedUpdate(){
+        //カメラのxとzのベクトルを抽出
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+ 
+        // 方向キーの入力値とカメラの向きから、移動方向を決定
+        Vector3 moveForward = cameraForward * rv.z + Camera.main.transform.right * rv.x;
+ 
+        // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+        rb.velocity = moveForward * speed + new Vector3(0, rb.velocity.y, 0);
+ 
+        // キャラクターの向きを進行方向に
+        if (moveForward != Vector3.zero) {
+            Player.transform.rotation = Quaternion.LookRotation(moveForward);
+            //Quaternion tx = Quaternion.Euler(moveForward);
+            //Quaternion tt = tx * Quaternion.AngleAxis(90, Vector3.up);
+            //Player.transform.rotation = Quaternion.RotateTowards(transform.rotation, tt, rb.velocity.x*90);
+        }
+
+    }
 }
