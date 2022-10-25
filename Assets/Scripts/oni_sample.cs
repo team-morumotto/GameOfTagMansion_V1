b0a80105a3b471e3d167df0e2df1167aa1f9fa14
fuchinosuke <wenmrykl_1218@yahoo.co.jp>
2022-10-25 11:01:09 +0900

    鬼動くようにしました

--- Assets/Scripts/oni_sample.cs
+++ Assets/Scripts/oni_sample.cs
@@ -5,16 +5,25 @@ using Photon.Pun;
 
 public class oni_sample : MonoBehaviourPunCallbacks
 {
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!photonView.IsMine){
+            return;
+        }
+        var speed = 5f;
+        var vx = Input.GetAxis("Horizontal") * speed;
+        var vz = Input.GetAxis("Vertical") * speed;
+
+        rb.velocity = new Vector3(vx,rb.velocity.y,vz);
+
     }
     void OnCollisionEnter(Collision col){
         if(col.gameObject.GetComponent<PhotonView>() == false){
@@ -24,8 +33,8 @@ public class oni_sample : MonoBehaviourPunCallbacks
 
     }
 
-    [PunRPC]
+    /*[PunRPC]
     private void RpcSendMessage(string message) {
         Debug.Log(message);
-    }
+    }*/
 }
