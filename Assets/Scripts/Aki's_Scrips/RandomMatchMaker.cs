b9dd69b8fe88cfb843f826ba6586ebf888c82e13
waya <wayamoti2015@gmail.com>
2022-10-26 01:24:04 +0900

    RandomMatchMakersの添削

--- Assets/Scripts/Aki's_Scrips/RandomMatchMaker.cs
+++ Assets/Scripts/Aki's_Scrips/RandomMatchMaker.cs
@@ -22,68 +22,69 @@ using Cinemachine;
 public class RandomMatchMaker : MonoBehaviourPunCallbacks
 {
     // インスペクターから設定
-    public GameObject OniObject;//鬼オブジェクト
-    public GameObject PlayerObject;//プレイヤーオブジェクト
-    public GameObject[] SpawnPoint;//キャラクタースポーンポイント
-    
-    public GameObject[] Character = {null,null,null,null};//生成したキャラクターを格納する配列
-    private int Number;//鬼側か逃げる側かを識別するナンバー
-    public static int i = -1;//ナンバー
+    public GameObject[] OniObject = {null,null,null};		 //鬼オブジェクト
+    public GameObject[] PlayerObject = {null,null,null};	//プレイヤーオブジェクト
+    public GameObject[] SpawnPoint;							 //キャラクタースポーンポイント
+    public GameObject[] Character = {null,null,null,null};	//生成したキャラクターを格納する配列
+    public GameObject MasterConfig;							//マスターコンフィグオブジェクト
+    public Text Text;
+    private int Number;										//鬼側か逃げる側かを識別するナンバー
+    public static int i = -1;								 //ナンバー
     public float currentTime;
     public static bool GameStartFlg = false;
     public static bool CharacterSpawnFlg =false;
-    public Text Text;
-    public GameObject MasterConfig;//マスターコンフィグオブジェクト
-    void Update(){
-        if(SetName.onEndEditFLG){//SetNameスクリプトの名前入力後フラグがtrueになったらConnect関数を実行
+    void Update() {
+        //SetNameスクリプトの名前入力後フラグがtrueになったらConnect関数を実行
+        if(SetName.onEndEditFLG) {
             Connect();
         }
     }
-    void Connect()//Photonマスターサーバー接続
-    {
-        Number = GotoGameScene.a;//GotoGameSceneから鬼か逃げる側かを識別するナンバーを受け取る
-        PhotonNetwork.ConnectUsingSettings();//Photonネットワークへの接続処理部分(これがないとフォトンは使用できない)
+
+	//Photonマスターサーバー接続
+    void Connect() {
+        Number = GotoGameScene.a;				//GotoGameSceneから鬼か逃げる側かを識別するナンバーを受け取る
+        PhotonNetwork.ConnectUsingSettings();	//Photonネットワークへの接続処理部分(これがないとフォトンは使用できない)
     }
 
-    //マスターサーバ？に接続した時に呼ばれるコールバック
-    public override void OnConnectedToMaster()
-    {
-        PhotonNetwork.JoinRandomRoom();//ランダムにルームに接続
+    //マスターサーバ？に接続した時
+    public override void OnConnectedToMaster() {
+        PhotonNetwork.JoinRandomRoom(); //ランダムにルームに接続
     }
 
-    public override void OnJoinedLobby()//ロビーへの入室
-    {
-        PhotonNetwork.JoinRandomRoom();//ランダムにルームに接続
+	//ロビーへの入室に失敗した場合
+    public override void OnJoinedLobby() {
+        PhotonNetwork.JoinRandomRoom(); //ランダムにルームに再接続
     }
 
-    public override void OnJoinRandomFailed(short returnCode, string message)//ルームに参加できなかった
-    {
-        RoomOptions roomOptions = new RoomOptions();//ルームをインスタンス化
-        roomOptions.MaxPlayers = 4;//ルーム接続の最大人数
-        PhotonNetwork.CreateRoom(null, roomOptions);//ルームを作成(ルームの名前を指定しない場合はnullを指定)
+	//ルームに参加できなかった場合
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        RoomOptions roomOptions = new RoomOptions();	//ルームをインスタンス化
+        roomOptions.MaxPlayers = 4;						//ルーム接続の最大人数
+        PhotonNetwork.CreateRoom(null, roomOptions);	//ルームを作成(ルームの名前を指定しない場合はnullを指定)
     }
 
-    public override void OnJoinedRoom()//ルームに参加
-    {
-        GameObject mainCamera = GameObject.FindWithTag("MainCamera");//シーン上のメインカメラを取得
-        GameObject CinemachineManager = GameObject.FindWithTag("MainCameraManager");//シーン上のメインカメラマネージャーを取得
-        CinemachineManager.GetComponent<Cinemachine.CinemachineFreeLook>().enabled = true;//メインカメラマネージャーのCinemachineFreeLookを有効にする
-        CinemachineManager.GetComponent<Cinemachine.CinemachineCollider>().enabled = true;//メインカメラマネージャーのCinemachineColliderを有効にする
+	//ルームに参加した時
+    public override void OnJoinedRoom() {
+        GameObject mainCamera = GameObject.FindWithTag("MainCamera");						//シーン上のメインカメラを取得
+        GameObject CinemachineManager = GameObject.FindWithTag("MainCameraManager");		//シーン上のメインカメラマネージャーを取得
+        CinemachineManager.GetComponent<Cinemachine.CinemachineFreeLook>().enabled = true;	//メインカメラマネージャーのCinemachineFreeLookを有効にする
+        CinemachineManager.GetComponent<Cinemachine.CinemachineCollider>().enabled = true;	//メインカメラマネージャーのCinemachineColliderを有効にする
         CinemachineFreeLook camera = CinemachineManager.GetComponent<CinemachineFreeLook>();//CinemachineFreeLookコンポーネントを取得
-        switch(Number){
+        switch(Number) {
             case 0:
                 GameObject Player = PhotonNetwork.Instantiate(PlayerObject.name,SpawnPoint[PhotonNetwork.CurrentRoom.PlayerCount-1].transform.position,Quaternion.identity,0);//Oniオブジェクトを生成
-                camera.Follow = Player.transform;//CinemachineFreeLookコンポーネント内のFollowにOniオブジェクトのtransformを設定
-                camera.LookAt = Player.transform;//CinemachineFreeLookコンポーネント内のLookAtにOniオブジェクトのtransformを設定
+                camera.Follow = Player.transform; //CinemachineFreeLookコンポーネント内のFollowにOniオブジェクトのtransformを設定
+                camera.LookAt = Player.transform; //CinemachineFreeLookコンポーネント内のLookAtにOniオブジェクトのtransformを設定
                 break;
             case 1:
                 GameObject Oni = PhotonNetwork.Instantiate(OniObject.name,SpawnPoint[PhotonNetwork.CurrentRoom.PlayerCount-1].transform.position,Quaternion.identity,0);//Oniオブジェクトを生成
-                camera.Follow = Oni.transform;//CinemachineFreeLookコンポーネント内のFollowにOniオブジェクトのtransformを設定
-                camera.LookAt = Oni.transform;//CinemachineFreeLookコンポーネント内のLookAtにOniオブジェクトのtransformを設定
+                camera.Follow = Oni.transform; //CinemachineFreeLookコンポーネント内のFollowにOniオブジェクトのtransformを設定
+                camera.LookAt = Oni.transform; //CinemachineFreeLookコンポーネント内のLookAtにOniオブジェクトのtransformを設定
                 break;
         }
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 2) {//ルームに入室している人数がルームの最大人数になったら
-            PhotonNetwork.CurrentRoom.IsOpen = false;//ルームを閉める
+		//ルームに入室している人数がルームの最大人数になったら
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2) {
+            PhotonNetwork.CurrentRoom.IsOpen = false; //ルームを閉める
             GameStartFlg = true;
         }
     }
