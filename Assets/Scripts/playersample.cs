cc123f8f3ac789d3f004432be8b7e0e33c419a22
waya <wayamoti2015@gmail.com>
2022-10-26 03:12:20 +0900

    Result表示とGoToTitleに連動

--- Assets/Scripts/playersample.cs
+++ Assets/Scripts/playersample.cs
@@ -25,12 +25,14 @@ public class playersample : MonoBehaviourPunCallbacks
     public Canvas Canvas;
 
     public CinemachineOrbitalTransposer camera;
+    private GameObject Panels;
     void Start () {
         anim = GetComponent<Animator> ();
         rb = GetComponent<Rigidbody>();
         col = GetComponent<CapsuleCollider> ();
         camera = GameObject.FindWithTag("MainCameraManager").GetComponent<Cinemachine.CinemachineOrbitalTransposer>();//メインカメラマネージャーのCinemachineFreeLookを有効にする
         speed = initSpeed;
+        Panels = GameObject.Find("/Canvas").transform.Find("Result_PanelList").gameObject;
     }
 
     void Update () {
@@ -43,19 +45,19 @@ public class playersample : MonoBehaviourPunCallbacks
             string aho = PhotonNetwork.CurrentRoom.Name;
             string gomi = PhotonNetwork.LocalPlayer.UserId.ToString();
             PhotonNetwork.LocalPlayer.NickName = SetName.NAME;   // 名前をセット(名前入力後にオブジェクト生成のため)
-            
+
             if(inputHorizontal==0 && inputVertical==0){
                 anim.SetFloat ("Speed", 0);//プレイヤーが移動してないときは走るアニメーションを止める
             }else{
                 anim.SetFloat ("Speed", 1);//プレイヤーが移動しているときは走るアニメーションを再生する
             }
-                
+
             anim.speed = animSpeed;
             currentBaseState = anim.GetCurrentAnimatorStateInfo (0);
 
             // カメラの方向から、X-Z平面の単位ベクトルを取得
             Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        
+
             // 方向キーの入力値とカメラの向きから、移動方向を決定
             Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
 
@@ -68,14 +70,15 @@ public class playersample : MonoBehaviourPunCallbacks
         }
     }
     public void OnCollisionEnter(Collision col){
+
         if(!photonView.IsMine){
             return;
         }
         if(col.gameObject.GetComponent<oni_sample>() == true){
-            ResultPanel.SetActive(true);
+            Panels.SetActive(true);
         }
     }
     public void DeadAfterGotoTitle(){
-        ResultPanel.SetActive(false);
+        Panels.SetActive(true);
     }
 }
