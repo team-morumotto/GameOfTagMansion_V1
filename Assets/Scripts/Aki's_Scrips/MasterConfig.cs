9b319209d26c9bdacfbfff304d25e24b905ff91b
aki2772 <anbeiqingzi@gmail.com>
2022-10-25 23:56:57 +0900

    ルーム人数が最大入室可能人数を超えたら参加しているオブジェクトをステージの指定位置に転移可能にした
    ・現状はデバッグ段階のため、最大入室人数は2人。
    ・スクリプト内の【Baka】は仮関数名、後ほど変更予定。
    ※PunRPCを用いて、同タイミングで、ルーム内オブジェクトにAllで座標を送っている

--- Assets/Scripts/Aki's_Scrips/MasterConfig.cs
+++ Assets/Scripts/Aki's_Scrips/MasterConfig.cs
@@ -1,33 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class MasterConfig : MonoBehaviourPunCallbacks
-{
-    public static GameObject SpawnPoint;
-    public float currentTime;
-    public Text Text;
-    // Start is called before the first frame update
-    void Start()
+namespace MasterConfig{
+    public class MasterConfig : MonoBehaviourPunCallbacks
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        GameStart();
-        Debug.Log(currentTime);
-
-        if(RandomMatchMaker.GameStartFlg == true){
-            GameStart();
+        public GameObject[] SpawnPoint;//キャラクターのステージスポーンポイント
+        public float currentTime;
+        public GameObject[] list = {null,null,null,null};
+        public Text Text;
+        // Start is called before the first frame update
+        void ArraySet(){
+            PhotonNetwork.ConnectUsingSettings();//Photonネットワークへの接続処理部分(これがないとフォトンは使用できない)
+            photonView.RPC(nameof(Baka),RpcTarget.All);
         }
-    }
+        void Update(){
+            if(RandomMatchMaker.GameStartFlg){
+                ArraySet();
+                RandomMatchMaker.GameStartFlg = false;
+            }
+        }
 
-    void GameStart(){
-        currentTime = PhotonNetwork.ServerTimestamp;
-        Text.text = currentTime.ToString();
+        [PunRPC]
+        void Baka(){
+            GameObject[] Character_Nige = GameObject.FindGameObjectsWithTag("Nigeru");//逃げる側のキャラクターを配列に格納
+            GameObject[] Character_Oni = GameObject.FindGameObjectsWithTag("Oni");//鬼側のキャラクターを配列に格納
+
+            Array.Copy(Character_Nige,list,Character_Nige.Length);//逃げる側のキャラクターを配列に格納
+            Array.Copy(Character_Oni,0,list,Character_Nige.Length,Character_Oni.Length);//逃げる側のキャラクターを配列に格納
+            for (int i = 0; i < 2; i++)
+            {
+                Debug.Log(i);
+                list[i].transform.position = SpawnPoint[i].transform.position;//キャラクターのスポーンポイントを設定
+            }
+        }
+        void GameStart(){
+            currentTime = PhotonNetwork.ServerTimestamp;
+            Text.text = currentTime.ToString();
+        }
     }
 }
