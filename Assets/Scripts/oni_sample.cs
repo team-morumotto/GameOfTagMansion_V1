8f0cec426922a7d70a327c57f0ea81e7c9ca8ebb
fuchinosuke <wenmrykl_1218@yahoo.co.jp>
2022-10-25 12:12:31 +0900

    鬼が動くようになったはず

--- Assets/Scripts/oni_sample.cs
+++ Assets/Scripts/oni_sample.cs
@@ -5,9 +5,20 @@ using Photon.Pun;
 
 public class oni_sample : MonoBehaviourPunCallbacks
 {
-    void FixedUpdate(){
+    private Rigidbody rb;
+    public float speed = 5f;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update(){
         if(photonView.IsMine){
             PhotonNetwork.LocalPlayer.NickName = SetName.NAME;   // 名前をセット(名前入力後にオブジェクト生成のため)
+            var h = Input.GetAxis ("Horizontal") * speed;				// 入力デバイスの水平軸をhで定義
+            var v = Input.GetAxis ("Vertical") * speed;				// 入力デバイスの垂直軸をvで定義
+            
+            rb.velocity = new Vector3(h, rb.velocity.y, v);
         }
     }
     void OnCollisionEnter(Collision col){
@@ -15,11 +26,10 @@ public class oni_sample : MonoBehaviourPunCallbacks
             return;
         }
         //photonView.RPC(nameof(RpcSendMessage), RpcTarget.All, p);
-
     }
 
-    [PunRPC]
+    /*[PunRPC]
     private void RpcSendMessage(string message) {
         Debug.Log(message);
-    }
+    }*/
 }
